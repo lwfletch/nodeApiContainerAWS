731ad1f400ce21b2eaab3fa73d4dd639bd366e41
Trey Fletcher <treyfletcher612@gmail.com>
2022-02-24 09:20:32 -0500

    edgesource endpoint

--- server.js
+++ server.js
@@ -11,5 +11,9 @@ app.get('/freddy', (req, res) => {
    res.send('Say hello to Fred');
 });
 
+// app.get('/edgesource', (req, res) => {
+//    res.send('Give a warm welcome to the guys.');
+// });
+
 const port = process.env.PORT || 8081;
 app.listen(port, () => console.log(`Listening on port ${port}..`));
