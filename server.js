353d96a7fd459deef74cf782de37ee445adfeb72
Trey Fletcher <treyfletcher612@gmail.com>
2022-02-24 09:00:19 -0500

    new endpoint, update config

--- server.js
+++ server.js
@@ -7,5 +7,9 @@ app.get('/', (req, res) => {
    res.send('Welcome to my API built in NodeJS by Trey Fletcher');
 });
 
+app.get('/freddy', (req, res) => {
+   res.send('Say hello to Fred');
+});
+
 const port = process.env.PORT || 8081;
 app.listen(port, () => console.log(`Listening on port ${port}..`));
