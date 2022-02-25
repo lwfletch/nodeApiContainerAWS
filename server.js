71684c85e7177346f4f37681c9a1d5d95f885ee1
Trey Fletcher <treyfletcher612@gmail.com>
2022-02-25 16:42:20 -0500

    new orb in config

--- server.js
+++ server.js
@@ -7,13 +7,13 @@ app.get('/', (req, res) => {
    res.send('Welcome to my API built in NodeJS by Trey Fletcher');
 });
 
-app.get('/freddy', (req, res) => {
-   res.send('Say hello to Fred');
-});
+// app.get('/freddy', (req, res) => {
+//    res.send('Say hello to Fred');
+// });
 
-app.get('/edgesource', (req, res) => {
-   res.send('Give a warm welcome to the guys.');
-});
+// app.get('/edgesource', (req, res) => {
+//    res.send('Give a warm welcome to the guys.');
+// });
 
 const port = process.env.PORT || 8081;
 app.listen(port, () => console.log(`Listening on port ${port}..`));
